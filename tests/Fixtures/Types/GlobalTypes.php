7338608e838c28c4522a2c8ba449fe07e109967c
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2026-08-12 20:08:24 +0800

    Add tuple type handling and tests for keyless tuples in ArrayAndListTypesTest

--- tests/Fixtures/Types/GlobalTypes.php
+++ tests/Fixtures/Types/GlobalTypes.php
@@ -6,6 +6,7 @@ namespace TypePHP\Tests\Fixtures\Types;
 
 /**
  * @phpstan-type SharedShape array{id: positive-int, name: non-empty-string}
+ * @phpstan-type SharedTupleShape array{list<positive-int>, non-empty-string}
  */
 class GlobalTypes
 {
