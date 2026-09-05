cb6e8d3c59e79a3518ecd8bdc990fb3a45a40d5d
Reymart A. Calicdan <reymart.calicdan06@gmail.com>
2026-09-06 01:44:49 +0800

    Remove manual debug checks from TypeError test in ClassConstKeyShapeTest

--- tests/TypeChecking/ArraysAndShapes/ClassConstKeyShapeTest.php
+++ tests/TypeChecking/ArraysAndShapes/ClassConstKeyShapeTest.php
@@ -35,14 +35,6 @@ describe('Class Constant Keys in Array Shapes (array{self::CONST_KEY: T})', func
     test('throws TypeError when array shape item with class constant key violates type contract', function () {
         $container = new ConstKeyContainer();
 
-        $manualCheck = \TypePHP\Internal\RuntimeTypeChecker::setupScope(
-            'TypePHP\Tests\Fixtures\Types\ConstKeyContainer::process',
-            ['payload' => ['user_id' => -5, 'user_role' => 'admin']],
-            $container
-        );
-        fwrite(STDERR, "[DEBUG] setupScope returned class: " . (is_object($manualCheck) ? get_class($manualCheck) : var_export($manualCheck, true)) . "\n");
-        fwrite(STDERR, "[DEBUG] instanceof ErrorMessage: " . var_export($manualCheck instanceof \TypePHP\Internal\Diagnostic\ErrorMessage, true) . "\n");
-
         expect(fn() => $container->process([
             'user_id' => -5,
             'user_role' => 'admin',
