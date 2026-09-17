06eaf6714d4506c8ec9e1a86b5b98be9d18d4a56
Reymart A. Calicdan <reymart.calicdan06@gmail.com>
2026-09-17 19:30:00 +0800

    Add beforeEach and afterEach hooks to reset Config in Pest tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,3 +1,14 @@
 <?php
 
 declare(strict_types=1);
+
+use TypePHP\Internal\Util\Config;
+
+uses()
+    ->beforeEach(function () {
+        Config::reset();
+    })
+    ->afterEach(function () {
+        Config::reset();
+    })
+    ->in('Contract', 'Internal', 'Feature');
